773885d3780e8e60d10a1bf8f19d90949e002cf8
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-08-03 20:04:22 +0000

    Upload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,46 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  getRandomCard();
 };
+let palo = ["♦", "♥", "♠", "♣"];
+let cardNumber = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J",
+  "A"
+];
+
+let nuevaCarta = document.getElementById("btn");
+nuevaCarta.addEventListener("click", getRandomCard);
+
+function getRandomCard() {
+  let paloRandom = Math.floor(Math.random() * palo.length);
+  let cardNumberRandom = Math.floor(Math.random() * cardNumber.length);
+  let paloHeader = document.getElementById("paloHeader");
+  let numero = document.getElementById("numero");
+  let paloFooter = document.getElementById("paloFooter");
+  if (palo[paloRandom] == "♥" || palo[paloRandom] == "♦") {
+    paloHeader.className =
+      "card-header border-bottom-0 bg-white red text-start";
+    paloFooter.className = "red rotate";
+    numero.className = "card-body text-center align-middle my-auto red";
+  } else {
+    paloHeader.className = "card-header border-bottom-0 bg-white text-start";
+    paloFooter.className = "rotate";
+    numero.className = "card-body text-center align-middle my-auto";
+  }
+  paloHeader.innerHTML = palo[paloRandom];
+  numero.innerHTML = cardNumber[cardNumberRandom];
+  paloFooter.innerHTML = palo[paloRandom];
+}
+setInterval(getRandomCard, 5000);
